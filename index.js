fb2b5fb2a02b2ef211f44ad003fb9d80e3ba161c
Sean Febo <seanfebo@techie.com>
2018-03-19 05:55:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById("nested").querySelector("div.target")
+  return document.querySelector("#nested.target")
 }
 
 function increaseRankBy(n) {
