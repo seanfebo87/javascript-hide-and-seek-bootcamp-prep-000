e54688f4d21380a80f89e06309fbee823e7d083c
Sean Febo <seanfebo@techie.com>
2018-03-19 05:52:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  return document.getElementById("grand-node").querySelector(")
+  return document.getElementById("grand-node").querySelector("div div div div")
 }
